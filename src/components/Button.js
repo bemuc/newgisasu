fa2d90cbc4f6d1402b8eea4763878bdfc6550ae8
benito <muciroben@gmail.com>
2020-10-25 18:25:49 +0200

    little erreur

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,11 +6,11 @@ const STYLES =['btn--primary','btn--outline']
 const SIZES = ['btn-medium','btn--large']
 export const Button = ({children,type,onClick,buttonStyle,buttonSize}) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
-    const checkButtonSize =  SIZEs.includes(buttonSize) ?buttonSize : SIZEs[0]
+    const checkButtonSize =  SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
     return(
         <Link to='/sign-up' className='btn-mobile'>
-            <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onCLick} type={type}>
+            <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
                 {children}
             </button>
         </Link>
